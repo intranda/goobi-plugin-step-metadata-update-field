2f0138e8e6568f8e16a4fd2b4a04eebb7bbb7bf6
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2020-09-05 20:34:06 +0200

    updated name

--- plugin/src/main/java/de/intranda/goobi/plugins/MetadataUpdateFieldStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/MetadataUpdateFieldStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleStepPlugin implements IStepPluginVersion2 {
+public class MetadataUpdateFieldStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_sample";
+    private String title = "intranda_step_metadata-update-field";
     @Getter
     private Step step;
     @Getter
@@ -57,20 +57,17 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Sample step plugin initialized");
+        log.info("Metadata-update-field step plugin initialized");
     }
 
     @Override
     public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.FULL;
-        // return PluginGuiType.PART;
-        // return PluginGuiType.PART_AND_FULL;
-        // return PluginGuiType.NONE;
+        return PluginGuiType.NONE;
     }
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return "/uii/plugin_step_metadata-update-field.xhtml";
     }
 
     @Override
@@ -109,7 +106,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         boolean successfull = true;
         // your logic goes here
         
-        log.info("Sample step plugin executed");
+        log.info("Metadata-update-field step plugin executed");
         if (!successfull) {
             return PluginReturnValue.ERROR;
         }
